bfc6d0d313843db790f7e3bb2964e027b55ebafa
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-05-12 12:15:22 -0500

    removing _v2 files
    adding envGridArray

---
+++ R/envGridArray.R
@@ -0,0 +1,24 @@
+#' Environmental Grid Array. Returns an array of environmental
+#' combinations from a grid with a length out split.
+#' It can be used to get the probability of detection along
+#' a list of environmental time series
+#'
+#' @param envData A list with environmental raster time series
+#' @param lenght.out Number of grid splits
+#'
+#' @returns An array with environmental combinations from a grid
+#' @export
+#'
+#' @examples
+#' bio1 <- terra::unwrap(cmcc_cm_bio1)
+#' bio12 <- terra::unwrap(cmcc_cm_bio12)
+#' envData <- list(bio01 = bio1, bio12 = bio12)
+#' envGridArray(envData)
+envGridArray <- function(envData, lenght = 100) {
+  seqList <- function(x) seq(x[1], x[2], by = ((x[2] - x[1])/(lenght - 1)))
+  envDataRange <- Map(f = function(x) seqList(range(terra::minmax(x))), envData )
+  x_grid <- do.call(data.table::CJ, envDataRange)
+  envGridArray <- Map(function(x) matrix(x, ncol = 1), x_grid) |>
+    simplify2array()
+  envGridArray
+}
--- R/xsdm-package.R
+++ R/xsdm-package.R
@@ -1,4 +1,16 @@
 #' @useDynLib xsdm, .registration=TRUE
+#' @importFrom data.table data.table
+#' @importFrom data.table :=
+#' @importFrom data.table .SD
+#' @importFrom data.table .BY
+#' @importFrom data.table .N
+#' @importFrom data.table .I
+#' @importFrom data.table .GRP
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .EACHI
 #' @import Rcpp
 #' @importFrom RcppParallel RcppParallelLibs
 "_PACKAGE"
+
+
+
