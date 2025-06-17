ffaaf86efc7462fed8409c6fe2b095eab6746c53
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-06-16 19:08:41 -0500

    relax test. avoid duplicated points

--- tests/testthat/test-xsdm.R
+++ tests/testthat/test-xsdm.R
@@ -8,15 +8,16 @@ testthat::test_that("errors",{
   testthat::expect_error(
     xsdm(bio1_ts, occ = pts)
   )
-
-  bio1_ts <- terra::unwrap(cmcc_cm_bio1)
-  envData <- list(bio1 = bio1_ts)
-  pts <- mus_virtualis
-  pts <- rbind(pts,pts)
-  testthat::expect_error(
-    xsdm(envData, occ = pts),
-    regexp = "points are duplicated"
-  )
+#
+#   bio1_ts <- terra::unwrap(cmcc_cm_bio1)
+#   envData <- list(bio1 = bio1_ts)
+#   pts <- mus_virtualis
+#   pts <- rbind(pts,pts)
+#
+#    testthat::expect_error(
+#     xsdm(envData, occ = pts),
+#     regexp = "points are duplicated"
+#   )
 
 
 })
