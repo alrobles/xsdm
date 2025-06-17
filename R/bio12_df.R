d770aa191d06ef392ab8de03fe8828e05db58518
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-06-17 12:46:31 -0500

    create bio12 data frame

---
+++ R/bio12_df.R
@@ -0,0 +1,6 @@
+#' @title cmcc_cm bio1 time serires
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with time series with 30 years of bio01 data
+#' @details The data comes from chelsav1 time serires for
+#' cmcc_cm model. We compute bio12 (Anual precipitation) for a 30 years time series.
+"bio12_df"
